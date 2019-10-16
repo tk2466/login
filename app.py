e07d528bae10b7f6443b0b006fa66964c2d1a9b8
tk2466 <55135576+tk2466@users.noreply.github.com>
2019-10-16 10:55:54 -0400

    included success ID again

--- app.py
+++ app.py
@@ -8,23 +8,26 @@ import subprocess
 from subprocess import check_output
 from flask_wtf.csrf import CSRFProtect
 
+# User Variable to store entries
+Users = {}
 
-#User Variable to store entries
-Users = { }
 
 class RegistrationForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     password = PasswordField('New Password', [
-            validators.DataRequired(),
-            validators.length(min=6, max=20)
-        ])
+        validators.DataRequired(),
+        validators.length(min=6, max=20)
+    ])
     mfa = StringField('mfa', [validators.DataRequired(), validators.Length(min=10, max=20)])
+    success = StringField('success')
+
 
 class UserLoginForm(Form):
     username = StringField('Username', [validators.DataRequired()])
     password = PasswordField('Password', [validators.DataRequired()])
     mfa = StringField('mfa', [validators.DataRequired()])
     result = StringField('result')
+
 
 class SpellCheckForm(Form):
     inputtext = StringField(u'inputtext', widget=TextArea())
@@ -36,15 +39,17 @@ app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'memcached'
 app.config['SECRET_KEY'] = 'super secret key'
 
-#Login Manager
+# Login Manager
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 
-#CSRF Protect
-#csrf = CSRFProtect(app)
+
+# CSRF Protect
+# csrf = CSRFProtect(app)
 
 class User(flask_login.UserMixin):
     pass
+
 
 @login_manager.user_loader
 def user_loader(username):
@@ -53,6 +58,7 @@ def user_loader(username):
     user = User()
     user.id = username
     return user
+
 
 @login_manager.request_loader
 def request_loader(request):
@@ -74,7 +80,7 @@ def mainpage(user=None):
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
-    if request.method ==  'POST' and form.validate():
+    if request.method == 'POST' and form.validate():
         username = form.username.data
         password = sha256_crypt.encrypt(form.password.data)
         mfa = form.mfa.data
@@ -83,31 +89,34 @@ def register():
             return reinder_template('register.html', form=form)
         Users[username] = {'password': password, 'mfa': mfa}
         return redirect('/login')
+    form.success.data = 'success'
     return render_template('register.html', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = UserLoginForm(request.form)
     if request.method == 'POST':
-       username = form.username.data
-       password = form.password.data
-       mfa = form.mfa.data
-       if (username not in Users):
-           form.result.data = "incorrect"
-           return render_template('login.html', form=form)
-       if (not sha256_crypt.verify(password, Users[username]['password'])):
-           form.result.data = "incorrect"
-           return render_template('login.html', form=form)
-       if (mfa != Users[username]['mfa']):
-           form.result.data = "Two-factor failure"
-           return render_template('login.html', form=form)
-       user = User()
-       user.id = username
-       flask_login.login_user(user)
-       form.result.data = "success"
-       #return redirect('/spell_check')
+        username = form.username.data
+        password = form.password.data
+        mfa = form.mfa.data
+        if (username not in Users):
+            form.result.data = "incorrect"
+            return render_template('login.html', form=form)
+        if (not sha256_crypt.verify(password, Users[username]['password'])):
+            form.result.data = "incorrect"
+            return render_template('login.html', form=form)
+        if (mfa != Users[username]['mfa']):
+            form.result.data = "Two-factor failure"
+            return render_template('login.html', form=form)
+        user = User()
+        user.id = username
+        flask_login.login_user(user)
+        form.result.data = "success"
+        # return redirect('/spell_check')
     return render_template('login.html', form=form)
-           
+
+
 @app.route('/spell_check', methods=['GET', 'POST'])
 @login_required
 def spell_check():
@@ -116,7 +125,7 @@ def spell_check():
         inputtext = form.inputtext.data
         form.textout.data = inputtext
         with open("words.txt", "w") as fo:
-            fo.write(inputtext)      
+            fo.write(inputtext)
         output = (check_output(["./a.out", "words.txt", "wordlist.txt"], universal_newlines=True))
         form.misspelled.data = output.replace("\n", ", ").strip().strip(',')
     response = make_response(render_template('spell_check.html', form=form))
