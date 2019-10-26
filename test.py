6e13997e453b287ed6862c99aaf1d08eedda01d2
tk2466 <55135576+tk2466@users.noreply.github.com>
2019-10-26 17:57:01 -0400

    added beautiful soup

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import unittest
 import requests
 from app import app
+from bs4 import BeautifulSoup
 
-server_address="http://127.0.0.1:5000"
-SERVICE_ADDR=server_address
+
+server_address = "http://127.0.0.1:5000"
 
 
 class FeatureTest(unittest.TestCase):
@@ -25,45 +26,44 @@ class FeatureTest(unittest.TestCase):
         self.assertEqual(req.status_code, 401)
 
     def test_spell_check(self):
-        
         headers = {'User-Agent': 'My User Agent'}
         s = requests.Session()
-        req = s.get(server_address + "/register")     
+        req = s.get(server_address + "/register")
         headers['cookie'] = '; '.join([x.name + '=' + x.value for x in req.cookies])
         headers['content-type'] = 'application/x-www-form-urlencoded'
-        #print(req.content)
         self.assertEqual(req.status_code, 200)
-        uname = 'kthava'
-        pword = "heart2017"
-        mfa = "7328289002"
+        uname = 'thava'
+        pword = "1981228jothy"
+        mfa = "9084101095"
         req = s.post(server_address + "/register", data=dict(
             uname=uname, pword=pword, mfa=mfa), headers=headers
-        )
+                     )
+
         req = s.get(server_address + "/login")
         headers['cookie'] = '; '.join([x.name + '=' + x.value for x in req.cookies])
         headers['content-type'] = 'application/x-www-form-urlencoded'
         req = s.post(server_address + "/login", data=dict(
             uname=uname, pword=pword, mfa=mfa), headers=headers
-        )                  
+                     )
+
         self.assertEqual(req.status_code, 200)
         req = s.get(server_address + "/spell_check")
         self.assertEqual(req.status_code, 200)
-        inputtext = "one two chalfdae three four blakc-da"
+        inputtext = "lion is not the kiing of jjungle"
         req = s.post(server_address + "/spell_check", data=dict(
             inputtext=inputtext)
-        )                  
-        print(req.content)
-        text = b'chalfdae, blakc-da'
-        if text in req.content:
-            print("OK Spelling")
-        else:
-            self.assertEqual(1, 0)
-
-
+                     )
+        soup = BeautifulSoup(req.content, 'html.parser')
+        misspelled = soup.find("p", {"id": "misspelled"})
+        textout = soup.find("p", {"id": "textout"})
+        mspelled = 'kiing, jjungle'
+        tout = "lion is not the kiing of jjungle"
+        self.assertEqual(mspelled, misspelled.text)
+        self.assertEqual(tout, textout.text)
 
     def tearDown(self):
         pass
 
+
 if __name__ == '__main__':
-    
     unittest.main()
